adb10ff8ca3bcb8e22ab4fd6c61a11e05403dee0
陈鑫旺 <15117500955@163.com>
2024-09-20 16:53:28 +0800

    添加lottie渲染json动画

--- desk/src/views/Home.vue
+++ desk/src/views/Home.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="Home-content">
-
+  <div>
+    <lottieVue class="lottie" :lottieData="lottieData" />
   </div>
 </template>
 
-<script setup>
+<script>
+import { ref } from "vue";
+import lottieVue from "../components/lottie.vue";
+import Animationdata from "../assets/Animation - 1726819559998.json?url";
 
+export default {
+  components: {
+    lottieVue,
+  },
+  setup() {
+    const lottieData = ref({
+      path: Animationdata,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+    });
+
+    return {
+      lottieData,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-.Home-content {
-  width: 100vw;
-  height: 100vh;
-  background-color: rgb(38, 12, 46);
+<style scoped>
+.lottie {
+  width: 500px;
+  height: 500px;
 }
 </style>
