645e49a5170ad544fb81300a744fe15082a7b5b1
陈鑫旺 <15117500955@163.com>
2024-09-20 14:04:55 +0800

    卸载viewUI、调整结构

---
+++ desk/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Home-content">
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.Home-content {
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(38, 12, 46);
+}
+</style>
