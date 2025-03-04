bdd78ead418214a0b9bbf90a374bb830b4b8bf1b
陈鑫旺 <15117500955@163.com>
2025-03-04 11:18:21 +0800

    更换项目结构-简历平台

--- desk/src/router/index.js
+++ desk/src/router/index.js
@@ -1,20 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import hemo from '@/views/hemo.vue'
-import three from '@/recovery/three.vue'
-const router = createRouter({
+
+const Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: hemo
-    },
-    {
-      path: '/three',
-      name: 'three',
-      component: three
+      name: 'index',
+      component: ()=> import('@/views/index.vue')
     },
   ]
 })
 
-export default router
+export default Router
--- desk/vite.config.js
+++ desk/vite.config.js
@@ -1,9 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import path from 'path';
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0', // 监听所有网络接口
@@ -16,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src')
     }
   },
 })
